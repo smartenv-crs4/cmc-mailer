c19d084302db2a0b9e9b883cde462db1740f3a03
Alessandro Romanino <a.romanino@gmail.com>
2017-06-28 10:17:23 +0200

    CMC Mailer Microservice license error fix

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@
  *               GNU General Public License for more details.                 *
  *                                                                            *
  *     You should have received a copy of the GNU General Public License      *
- *    along with CMC-Maileer.  If not, see <http://www.gnu.org/licenses/>.    *
+ *    along with CMC-Mailer.  If not, see <http://www.gnu.org/licenses/>.    *
  * ############################################################################
  */
 
